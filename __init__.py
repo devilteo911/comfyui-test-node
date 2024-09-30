8f585a948859165adedb1cee57a8479354a844e9
devilteo911 <matteomoro996@gmail.com>
2024-09-30 21:40:53 +0200

    Add PrintHelloWorld node and map it in __init__.py.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .nodes.test import *
+
+NODE_CLASS_MAPPINGS = {
+    # Add mappings here
+    "Print Hello World": PrintHelloWorld,
+}
+
+print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")
